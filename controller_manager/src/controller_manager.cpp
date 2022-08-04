7f6a1ef25a8ca0ff6818b83b21adb82d0f2a0e44
rbordalba <rbordalba@gmail.com>
2022-08-04 18:03:11 +0200

    controller update_rate different from controller manager update_rate

--- controller_manager/src/controller_manager.cpp
+++ controller_manager/src/controller_manager.cpp
@@ -1200,8 +1200,10 @@ controller_interface::return_type ControllerManager::update(
 {
   std::vector<ControllerSpec> & rt_controller_list =
     rt_controllers_wrapper_.update_and_get_used_by_rt_list();
-
+  
   auto ret = controller_interface::return_type::OK;
+  ++update_loop_counter_;
+  update_loop_counter_ %= update_rate_;
 
   for (auto loaded_controller : rt_controller_list)
   {
@@ -1211,14 +1213,33 @@ controller_interface::return_type ControllerManager::update(
     {
       auto controller_update_rate = loaded_controller.c->get_update_rate();
 
-      auto controller_ret = loaded_controller.c->update(
-        time, (controller_update_rate != update_rate_ && controller_update_rate != 0)
-                ? rclcpp::Duration::from_seconds(1.0 / controller_update_rate)
-                : period);
+      unsigned int update_ratio;
+      if (controller_update_rate == 0)
+        update_ratio = 1;
+      else
+        update_ratio = (unsigned int) update_rate_/controller_update_rate;
+  
+      if (update_ratio < 1 )
+        update_ratio = 1;
+        
+      bool controller_go =
+        controller_update_rate == 0 || ((update_loop_counter_ % update_ratio) == 0);
+      RCLCPP_DEBUG(
+        get_logger(), "update_loop_counter: '%d ' controller_go: '%s ' controller_name: '%s '",
+        update_loop_counter_, controller_go ? "True" : "False",
+        loaded_controller.info.name.c_str());
 
-      if (controller_ret != controller_interface::return_type::OK)
+      if (controller_go)
       {
-        ret = controller_ret;
+        auto controller_ret = loaded_controller.c->update(
+          time, (controller_update_rate != update_rate_ && controller_update_rate != 0)
+                  ? rclcpp::Duration::from_seconds(1.0 / controller_update_rate)
+                  : period);
+
+        if (controller_ret != controller_interface::return_type::OK)
+        {
+          ret = controller_ret;
+        }
       }
     }
   }
